ce8d90dcce0641fb56960f1ad7ca69639caa7b9d
Jeremiah Griffin <jgriffin@ciedigital.com>
2024-08-12 16:49:00 -0400

    cleanup

--- Todo/Interfaces/IEndpoint.cs
+++
@@ -1,9 +0,0 @@
-namespace Todo.Interfaces;
-
-/// <summary>
-/// A module is a collection of endpoints that are grouped together
-/// </summary>
-public interface IModule
-{
-    public void MapEndpoints(IEndpointRouteBuilder endpoints);
-}
